4377824aeba91db72d5ed8b630014df1e94c455f
Shishir Singh <shishirsinghvaranasi@gmail.com>
2021-12-17 01:48:23 +0530

    search message to users

--- src/core/Search.js
+++ src/core/Search.js
@@ -59,7 +59,7 @@ const Search = () => {
                 <div className="input-group input-group-lg">
                     <div className="input-group-prepend">
                         <select className="btn mr-2" onChange={handleChange('category')}>
-                            <option value="All"> Pick category</option>
+                            <option value="All"> All </option>
                             {categories.map((c,i) => (<option key={i} value={c._id}> {c.name} </option>) )}
                         </select>
                     </div>
@@ -80,11 +80,24 @@ const Search = () => {
         </form>
     );
 
+    const searchMessage = (searched, results) => {
+        if (searched && results.length > 0){
+            return `Found ${results.length} products`;
+        }
+        if (searched && results.length == 0){
+            return 'No products found'
+        }
+    }
+
     const searchedProducts = (results = []) => {
         return (
-            <div className="row">
-                {results.map((product,i) => (<Card key={i} product={product}/>))}
+            <div>
+                <h2 className="mt-4 mb-4"> {searchMessage(searched, results)} </h2>
+                <div className="row">
+                    {results.map((product,i) => (<Card key={i} product={product}/>))}
+                </div>
             </div>
+        
         )
     }
 
