7a6512665d0e265b527006c0833dc4371e3ef9b5
Shishir Singh <shishirsinghvaranasi@gmail.com>
2021-12-20 13:53:08 +0530

    save orders in database

--- src/core/Checkout.js
+++ src/core/Checkout.js
@@ -59,53 +59,61 @@ const Checkout = ({products,setRun = f => f, run = undefined}) => {
         setData({loading: true});
         let nonce;
         let getNonce = data.instance.requestPaymentMethod().then(data => {
-           // console.log(data);
-            nonce = data.nonce
-            // once u have nonce (card type, card no.) send nonce as 'paymentMethodNonce' and also total to be charged
-            // console.log('nonce and total amt: ', nonce, getTotal(products))
-            const paymentData = {
-                paymentMethodNonce: nonce,
-                amount: getTotal(products)
-            }
+                    // console.log(data);
+                    nonce = data.nonce
+                    // once u have nonce (card type, card no.) send nonce as 'paymentMethodNonce' and also total to be charged
+                    // console.log('nonce and total amt: ', nonce, getTotal(products))
+                    const paymentData = {
+                        paymentMethodNonce: nonce,
+                        amount: getTotal(products)
+                    }
 
-            processPayment(userId, token, paymentData)
-            .then(response => {
-                console.log('processPayment response', response);
-                setData({...data, success: response.success})
-                // empty order
-                // create order: post req to backend to create order 
-                // clear the cart from the localstorage
-                
-                const createOrderData = {
-                    products: products,
-                    transaction_id: response.transaction.id,
-                    amount: response.transaction.amount,
-                    address: data.address
-                }
+                    processPayment(userId, token, paymentData)
+                    .then(response => {
+                        console.log(response);
+                        // empty cart
+                        // create order
 
-                createOrder(userId, token, createOrderData);
+                        const createOrderData = {
+                            products: products,
+                            transaction_id: response.transaction.id,
+                            amount: response.transaction.amount,
+                            address: data.address
+                        };
 
-                emptyCart(() => console.log('empty the cart'));
-                setRun(!run); // run useEffect in parent Cart
-                setData({loading: false});
-            })
-            .catch(err => {
-                setData({loading: false});
-                console.log(err)
-            })
-            
-        })
-        .catch(error => {
-            //console.log('Drop-in error: ', error);
-            setData({...data, error: error.message})
-        })
-    }
+                        createOrder(userId, token, createOrderData)
+                            .then(response => {
+                                emptyCart(() => {
+                                    setRun(!run); // run useEffect in parent Cart
+                                    console.log('payment success and empty cart');
+                                    setData({
+                                        loading: false,
+                                        success: true
+                                    });
+                                });
+                            })
+                            .catch(error => {
+                                console.log(error);
+                                setData({ loading: false });
+                            });
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        setData({ loading: false });
+                    })
+            }) 
+            .catch(error => {
+                // console.log("dropin error: ", error);
+                setData({ ...data, error: error.message });
+            });
+        };
 
-    const showError = error => {
-       return <div className="alert alert-danger" style={{display: error ? '' : 'none'}}> 
+
+    const showError = error => (
+       <div className="alert alert-danger" style={{display: error ? '' : 'none'}}> 
             {error}
         </div>
-    }
+    )
 
     const showSuccess = success => {
         return <div className="alert alert-info" style={{display: success ? '' : 'none'}}> 
@@ -113,7 +121,7 @@ const Checkout = ({products,setRun = f => f, run = undefined}) => {
          </div>
      }
 
-    const showLoading = loading => loading && <h2>Loading...</h2>
+    const showLoading = loading => loading && <h2 className="alert alert-danger">Loading...</h2>
 
     const handleAddress = e => {
         setData({...data, address: e.target.value})
